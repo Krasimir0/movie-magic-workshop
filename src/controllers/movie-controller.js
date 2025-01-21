6ad94caf019307c87a18ed1657c7e6624c7e7452
Krasimir Petranov <kimim0548@gmail.com>
2025-01-21 23:17:58 +0000

    Parse data on server

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -6,12 +6,20 @@ movieController.get('/create', (req, res) =>
 {
     res.render('create');
 });
+
+movieController.post('/create', (req, res) => {
+    const newMovie = req.body;
+    console.log(newMovie);
+        
+    res.end();
+})
+
 movieController.get('/:movieId/details', (req, res) =>
     {
         const movieId = req.params.movieId;
-
         const movie = movieService.findMovie(movieId);
-        res.render('details');
+        
+        res.render('details', {movie});
     });
 
 export default movieController;
--- src/movies.js
+++ src/movies.js
@@ -5,7 +5,22 @@ export default [
     genre: 'Adventure',
     description:
       'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
-    imageUrl: './static/img/jungle-cruise.jpeg'
+    imageUrl: '/static/img/jungle-cruise.jpeg',
+    director: 'Jaume Collet-Serra',
+      year: 2021,
+      rating: 6.6,
+      category: 'Movie'
     },
+    {
+      id: 2,
+      title: 'The Amazing Spider-Man',
+      genre: 'Superhero',
+      description: 'After Peter Parker is bitten by a genetically altered spider, he gains newfound, spider-like powers and ventures out to save the city from the machinations of a mysterious reptilian foe.',
+      imageUrl: 'https://www.destroythebrain.com/wp-content/uploads/2012/07/the-amazing-spider-man-new-poster-scaled.jpg',
+      director: 'Marc Webb',
+      year: 2012,
+      rating: 6.9,
+      category: 'Movie'
+    }
 
 ];
