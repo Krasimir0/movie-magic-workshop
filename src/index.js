5a208b76f2eb12fac77eec155670a69dd6c6acad
Krasimir Petranov <kimim0548@gmail.com>
2025-01-21 18:48:50 +0000

    add routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
-import homeController from './controllers/home-controller.js';
+import routes from './routes.js'
+
 const app = express();
 
 app.engine('hbs', handlebars.engine({
@@ -11,11 +12,7 @@ app.set('views',  './src/views')
 
 app.use('/static', express.static('./src/public'))
 
-app.use(homeController)
-
-app.get('*', (req, res) => {
-    res.render('404')
-})
+app.use(routes);
 
 app.listen(3000, () => console.log("Server is listening on port http://localhost:3000...")
 )
