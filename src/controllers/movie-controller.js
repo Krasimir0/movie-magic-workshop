aa878f11924579b13f6dca1ae4f10f1e8b955343
Krasimir Petranov <kimim0548@gmail.com>
2025-01-22 01:14:37 +0000

    add rendering from movies

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -22,4 +22,9 @@ movieController.get('/:movieId/details', (req, res) =>
         res.render('details', {movie});
     });
 
+movieController.get('/search', (req, res) => {
+    const movies = movieService.getAll();
+    res.render('search', { movies });
+})
+
 export default movieController;
