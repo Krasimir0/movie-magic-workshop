6cee8990991979822c8961d19381c4b5d789b5a9
Krasimir Petranov <kimim0548@gmail.com>
2025-02-03 17:15:14 +0000

    Fix navigation

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,7 @@ const routes = Router();
 routes.use(homeController);
 routes.use('/movies', movieController);
 routes.use('/casts', castController)
-router.use('/auth', authController)
+routes.use('/auth', authController)
 
 routes.get('*', (req, res) => {
     res.render('404')
