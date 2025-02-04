d38f5011007a7bc469c561190e8a4ebda964c1f3
Krasimir Petranov <kimim0548@gmail.com>
2025-02-04 17:17:09 +0000

    Add dynamic navigation

--- src/middlewares/auth-middleware.js
+++ src/middlewares/auth-middleware.js
@@ -13,6 +13,7 @@ export const authMiddleware = (req, res, next) => {
         const decodedToken = jwt.verify(token, SECRET);
         //Attach decoded token to request
         req.user = decodedToken;
+        res.locals.user = decodedToken;
         next();
     } catch (err) {
         res.clearCookie('auth');
