db29af4bb4d850def5a9a9e34995ee3ca1e94926
Krasimir Petranov <kimim0548@gmail.com>
2025-02-04 22:52:05 +0000

    Add edit page with hardcoded data

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -3,61 +3,62 @@ import movieService from "../services/movie-service.js";
 import castService from "../services/cast-service.js";
 const movieController = Router();
 
-movieController.get('/create', (req, res) =>
-{
-    res.render('create');
+movieController.get("/create", (req, res) => {
+  res.render("create");
 });
 
-movieController.post('/create', async(req, res) => {
-    const newMovie = req.body;
-    const userId = req.user?.id; 
+movieController.post("/create", async (req, res) => {
+  const newMovie = req.body;
+  const userId = req.user?.id;
 
-   await movieService.create(newMovie, userId);    
-    res.redirect('/')
-})
-
-movieController.get('/:movieId/details', async (req, res) =>
-    {
-        const movieId = req.params.movieId;
-        const movie = await movieService.getOneWithCasts(movieId);
-        const isCreator = movie.creator && movie.creator?.equals(req.user?.id);
-        
-        res.render('movies/details', {movie, isCreator});
-    });
-
-movieController.get('/:movieId/delete', async (req,res) => {
-        const movieId = req.params.movieId;
-
-        const movie = await movieService.getOne(movieId);
-        if (!movie.creator?.equals(req.user?.id)) {
-            return res.redirect("/404");
-        }
-
-       await movieService.delete(movieId);
-       res.redirect("/");
+  await movieService.create(newMovie, userId);
+  res.redirect("/");
 });
 
-movieController.get('/search', async (req, res) => {
-    const searchQueries = req.query;
-    const movies = await movieService.getAll(searchQueries);
-    res.render('search', { movies, searchQueries });
-})
+movieController.get("/:movieId/details", async (req, res) => {
+  const movieId = req.params.movieId;
+  const movie = await movieService.getOneWithCasts(movieId);
+  const isCreator = movie.creator && movie.creator?.equals(req.user?.id);
 
-movieController.get('/:movieId/attach-cast', async (req, res) =>
-{
-    const movieId = req.params.movieId;
-    const movie = await movieService.getOne(movieId);
-    const casts = await castService.getAll({exclude: movie.casts}); 
-
-    res.render('movies/cast-attach', { movie, casts });
+  res.render("movies/details", { movie, isCreator });
 });
 
-movieController.post('/:movieId/attach-cast', async (req, res) => {
-        const castId = req.body.cast;
-        const movieId = req.params.movieId;
+movieController.get("/:movieId/delete", async (req, res) => {
+  const movieId = req.params.movieId;
 
-        await movieService.attachCast(movieId, castId);
-        res.redirect(`/movies/${movieId}/details`)
+  const movie = await movieService.getOne(movieId);
+  if (!movie.creator?.equals(req.user?.id)) {
+    return res.redirect("/404");
+  }
+
+  await movieService.delete(movieId);
+  res.redirect("/");
+});
+
+movieController.get("/:movieId/edit", (req, res) => {
+  res.render('movies/edit');
+});
+
+movieController.get("/search", async (req, res) => {
+  const searchQueries = req.query;
+  const movies = await movieService.getAll(searchQueries);
+  res.render("search", { movies, searchQueries });
+});
+
+movieController.get("/:movieId/attach-cast", async (req, res) => {
+  const movieId = req.params.movieId;
+  const movie = await movieService.getOne(movieId);
+  const casts = await castService.getAll({ exclude: movie.casts });
+
+  res.render("movies/cast-attach", { movie, casts });
+});
+
+movieController.post("/:movieId/attach-cast", async (req, res) => {
+  const castId = req.body.cast;
+  const movieId = req.params.movieId;
+
+  await movieService.attachCast(movieId, castId);
+  res.redirect(`/movies/${movieId}/details`);
 });
 
 export default movieController;
